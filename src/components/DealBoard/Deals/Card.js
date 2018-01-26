2c987184dd999ec3cc336b9b88ebd4debddc9b77
ygokirmak@gmail.com <git_231905>
2018-01-26 17:18:44 +0300

    - deal dto updated

--- src/components/DealBoard/Deals/Card.js
+++ src/components/DealBoard/Deals/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from "react-router-dom/es/Link";
+import moment from 'moment';
 
 const propTypes = {
     item: PropTypes.object.isRequired,
@@ -13,6 +14,8 @@ const propTypes = {
 const Card = (props) => {
     const { style, item } = props;
 
+    const formattedDate = moment(item.createdDate, "YYYY-MM-DDTHH:mm:ss+-HH:mm").format("DD.MM.YYYY");
+
     return (
 
         <li style={style} className="info-element" id={style ? item.id : null}>
@@ -21,7 +24,7 @@ const Card = (props) => {
                 <a href="#" className="pull-right btn btn-xs btn-primary">Done</a>
                 <Link to={"/deal/"+item.id}><i className="btn fa fa-edit" /></Link>
                 <i className="btn fa fa-trash" onClick={() => props.deleteDeal(item.id)}/>
-                <i className="fa fa-clock-o"/> 08.04.2015
+                <i className="fa fa-clock-o"/> {formattedDate}
             </div>
         </li>
     );
