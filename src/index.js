035ccb50770151530454e0dad7cccf3ccd42e490
ygokirmak@gmail.com <git_231905>
2018-02-26 22:44:00 +0300

    - fixes for google cloud deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './../node_modules/font-awesome/css/font-awesome.css'
 import './../node_modules/animate.css/animate.min.css'
 
 
