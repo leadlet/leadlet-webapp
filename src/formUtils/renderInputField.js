d3c851e37555a03bc1789f35c9f3fc05e8e5333a
KancerEzeroglu <ezeroglukancer@gmail.com>
2019-01-08 13:06:18 +0100

    fixed error

--- src/formUtils/renderInputField.js
+++ src/formUtils/renderInputField.js
@@ -5,7 +5,7 @@ const renderInputField = (props) => (
         <label>{props.label}</label>
         <input {...props.input} placeholder={props.label} type={props.type} className="form-control"/>
         <span style={{color: "red"}} className="help-block m-b-none">
-                {props.meta.touched && ((props.meta.error && <span>{props.meta.error}</span>))}
+                {(props.meta.error && <span>{props.meta.error}</span>)}
             </span>
     </div>
 );
