975fab64301a41f8f811e9bc5c8562318c7d6294
KancerEzeroglu <ezeroglukancer@gmail.com>
2019-04-23 13:20:30 +0200

    removed unnecassary comment

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -9,7 +9,6 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 export const store = createStore(
     rootReducer, composeWithDevTools(
     applyMiddleware(
-        //TODO kancer
         thunkMiddleware,
 //        loggerMiddleware
     ))
